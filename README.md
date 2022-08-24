c2b20682314b1a7addd12e967328ba21a1a9f2bc
Jacques Nadeau <jacquesnadeau@gmail.com>
2022-08-23 17:11:00 -0700

    init commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # substrait-cpp
+
+Planned home for CPP libraries to help build/consume Substrait query plans.
---
+++ scripts/setup-ubuntu.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Minimal setup for Ubuntu 20.04.
+set -eufx -o pipefail
+SCRIPTDIR=$(dirname "${BASH_SOURCE[0]}")
+source $SCRIPTDIR/setup-helper-functions.sh
+
+# Folly must be built with the same compiler flags so that some low level types
+# are the same size.
+CPU_TARGET="${CPU_TARGET:-avx}"
+export COMPILER_FLAGS=$(get_cxx_flags $CPU_TARGET)
+FB_OS_VERSION=v2022.07.11.00
+NPROC=$(getconf _NPROCESSORS_ONLN)
+DEPENDENCY_DIR=${DEPENDENCY_DIR:-$(pwd)}
+
+# Install all dependencies.
+sudo --preserve-env apt install -y \
+  g++ \
+  cmake \
+  ccache \
+  ninja-build \
+  checkinstall \
+  git \
+  libgtest-dev
+
+function run_and_time {
+  time "$@"
+  { echo "+ Finished running $*"; } 2> /dev/null
+}
+
+function prompt {
+  (
+    while true; do
+      local input="${PROMPT_ALWAYS_RESPOND:-}"
+      echo -n "$(tput bold)$* [Y, n]$(tput sgr0) "
+      [[ -z "${input}" ]] && read input
+      if [[ "${input}" == "Y" || "${input}" == "y" || "${input}" == "" ]]; then
+        return 0
+      elif [[ "${input}" == "N" || "${input}" == "n" ]]; then
+        return 1
+      fi
+    done
+  ) 2> /dev/null
+}
+
+
+function install_protobuf {
+  wget https://github.com/protocolbuffers/protobuf/releases/download/v21.4/protobuf-all-21.4.tar.gz
+  tar -xzf protobuf-all-21.4.tar.gz
+  cd protobuf-21.4
+  ./configure --prefix=/usr
+  make "-j$(nproc)"
+  make install
+  ldconfig
+}
+
+function install_velox_deps {
+  run_and_time install_protobuf
+}
+
+
+(return 2> /dev/null) && return # If script was sourced, don't run commands.
+
+
+(
+  if [[ $# -ne 0 ]]; then
+    for cmd in "$@"; do
+      run_and_time "${cmd}"
+    done
+  else
+    install_velox_deps
+  fi
+)
+
+
+
+echo "All deps installed! Now try \"make\""
