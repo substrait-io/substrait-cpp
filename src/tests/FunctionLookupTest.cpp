81cbf478a58644742c6c4e492ba93f9a18a62e48
Zhang, Chaojun <chaojun.zhang@intel.com>
2022-10-21 12:18:35 +0800

    passing functionSignature instead of name and arguments

--- src/tests/FunctionLookupTest.cpp
+++ src/tests/FunctionLookupTest.cpp
@@ -41,16 +41,17 @@ protected:
     FunctionMappingPtr mappings_ =
         std::make_shared<const VeloxFunctionMappings>();
     scalarFunctionLookup_ =
-        std::make_shared<FunctionLookup>(extension_, mappings_);
+        std::make_shared<ScalarFunctionLookup>(extension_, mappings_);
     aggregateFunctionLookup_ =
-        std::make_shared<FunctionLookup>(extension_, mappings_);
+        std::make_shared<AggregateFunctionLookup>(extension_, mappings_);
   }
 
   void testScalarFunctionLookup(const std::string &name,
                                 const std::vector<TypePtr> &arguments,
+                                const TypePtr &returnType,
                                 const std::string &outputSignature) {
-    const auto &functionVariant =
-        scalarFunctionLookup_->lookupScalarFunction(name, arguments);
+    const auto &functionVariant = scalarFunctionLookup_->lookupFunction(
+        FunctionSignature(name, arguments, returnType));
 
     ASSERT_TRUE(functionVariant != nullptr);
     ASSERT_EQ(functionVariant->signature(), outputSignature);
@@ -58,10 +59,11 @@ protected:
 
   void testAggregateFunctionLookup(const std::string &name,
                                    const std::vector<TypePtr> &arguments,
+                                   const TypePtr &returnType,
                                    const std::string &outputSignature) {
 
-    const auto &functionVariant =
-        scalarFunctionLookup_->lookupAggregateFunction(name, arguments);
+    const auto &functionVariant = aggregateFunctionLookup_->lookupFunction(
+        FunctionSignature(name, arguments, returnType));
 
     ASSERT_TRUE(functionVariant != nullptr);
     ASSERT_EQ(functionVariant->signature(), outputSignature);
@@ -73,50 +75,51 @@ private:
 };
 
 TEST_F(SubstraitFunctionLookupTest, compare_function) {
-  testScalarFunctionLookup("lt", {kI8(), kI8()}, "lt:any1_any1");
+  testScalarFunctionLookup("lt", {kI8(), kI8()}, kBool(), "lt:any1_any1");
 
-  testScalarFunctionLookup("lt", {kI16(), kI16()}, "lt:any1_any1");
+  testScalarFunctionLookup("lt", {kI16(), kI16()}, kBool(), "lt:any1_any1");
 
-  testScalarFunctionLookup("lt", {kI32(), kI32()}, "lt:any1_any1");
+  testScalarFunctionLookup("lt", {kI32(), kI32()}, kBool(), "lt:any1_any1");
 
-  testScalarFunctionLookup("lt", {kI64(), kI64()}, "lt:any1_any1");
+  testScalarFunctionLookup("lt", {kI64(), kI64()}, kBool(), "lt:any1_any1");
 
-  testScalarFunctionLookup("lt", {kFp32(), kFp32()}, "lt:any1_any1");
+  testScalarFunctionLookup("lt", {kFp32(), kFp32()}, kBool(), "lt:any1_any1");
 
-  testScalarFunctionLookup("lt", {kFp64(), kFp64()}, "lt:any1_any1");
-  testScalarFunctionLookup("between", {kI8(), kI8(), kI8()},
+  testScalarFunctionLookup("lt", {kFp64(), kFp64()}, kBool(), "lt:any1_any1");
+  testScalarFunctionLookup("between", {kI8(), kI8(), kI8()}, kBool(),
                            "between:any1_any1_any1");
 }
 
 TEST_F(SubstraitFunctionLookupTest, arithmetic_function) {
-  testScalarFunctionLookup("add", {kI8(), kI8()}, "add:opt_i8_i8");
+  testScalarFunctionLookup("add", {kI8(), kI8()}, kI8(), "add:opt_i8_i8");
 
-  testScalarFunctionLookup("plus", {kI8(), kI8()}, "add:opt_i8_i8");
+  testScalarFunctionLookup("plus", {kI8(), kI8()}, kI8(), "add:opt_i8_i8");
   testScalarFunctionLookup("divide",
                            {
                                kFp32(),
                                kFp32(),
                            },
-                           "divide:opt_opt_opt_fp32_fp32");
+                           kFp32(), "divide:opt_opt_opt_fp32_fp32");
 
   testAggregateFunctionLookup("avg", {Type::decode("struct<fp64,i64>")},
-                              "avg:opt_fp32");
+                              kFp32(), "avg:opt_fp32");
 }
 
 TEST_F(SubstraitFunctionLookupTest, avg) {}
 
 TEST_F(SubstraitFunctionLookupTest, logical) {
-  testScalarFunctionLookup("and", {kBool(), kBool()}, "and:bool");
-  testScalarFunctionLookup("or", {kBool(), kBool()}, "or:bool");
-  testScalarFunctionLookup("not", {kBool()}, "not:bool");
-  testScalarFunctionLookup("xor", {kBool(), kBool()}, "xor:bool_bool");
+  testScalarFunctionLookup("and", {kBool(), kBool()}, kBool(), "and:bool");
+  testScalarFunctionLookup("or", {kBool(), kBool()}, kBool(), "or:bool");
+  testScalarFunctionLookup("not", {kBool()}, kBool(), "not:bool");
+  testScalarFunctionLookup("xor", {kBool(), kBool()}, kBool(), "xor:bool_bool");
 }
 
 TEST_F(SubstraitFunctionLookupTest, string_function) {
-  testScalarFunctionLookup("like", {kString(), kString()}, "like:opt_str_str");
+  testScalarFunctionLookup("like", {kString(), kString()}, kBool(),
+                           "like:opt_str_str");
   testScalarFunctionLookup(
       "like", {Type::decode("varchar<L1>"), Type::decode("varchar<L2>")},
-      "like:opt_vchar<L1>_vchar<L2>");
-  testScalarFunctionLookup("substr", {kString(), kI32(), kI32()},
-                           "substring:str_i64_i64");
+      kBool(), "like:opt_vchar<L1>_vchar<L2>");
+  testScalarFunctionLookup("substr", {kString(), kI32(), kI32()}, kString(),
+                           "substring:str_i32_i32");
 }
