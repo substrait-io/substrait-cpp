5d69321c138bae72e3489118a777d4dc1d60397d
David Sisson <EpsilonPrime@users.noreply.github.com>
2023-04-11 22:19:33 -0700

    Made run-cmake-format.sh compatible with CLion's CMake setup.

--- scripts/run-cmake-format.sh
+++ scripts/run-cmake-format.sh
@@ -3,5 +3,5 @@
 SCRIPTDIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 WORKDIR="$( cd $SCRIPTDIR/.. && pwd )"
 
-find  $WORKDIR -type d \( -path ./third_party \) -prune -o \( -name '*.cmake' -o -name 'CMakeLists.txt' \) | grep -v "third_party" | xargs cmake-format -i
+find  $WORKDIR -type d \( -path ./third_party \) -prune -o \( -name '*.cmake' -o -name 'CMakeLists.txt' \) | grep -v "third_party" | grep -v "/.cmake" | xargs cmake-format -i
 cmake-format -i  $WORKDIR/third_party/CMakeLists.txt
