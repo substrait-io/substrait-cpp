d995094f59c90c9cbd9f0d25268e7082a8a1c107
David Sisson <EpsilonPrime@users.noreply.github.com>
2023-06-01 12:27:00 -0700

    fix: detect when maps are missing keys and/or values (#65)
    Now that the underlying type library doesn't segfault when given a map
    missing a key or value, properly detect such cases and return errors.

--- src/substrait/textplan/parser/tests/TextPlanParserTest.cpp
+++ src/substrait/textplan/parser/tests/TextPlanParserTest.cpp
@@ -652,7 +652,10 @@ std::vector<TestCase> getTestCases() {
             expression {}_list<string>?;
             expression {}_struct<a>;
             expression {}_struct<>;
-            //expression {}_map<>;  // TODO - Catch this behavior.
+            expression {}_map<>;
+            expression {}_map<string>;
+            expression {}_map<,string>;
+            expression {}_map<,>;
             expression {}_list<>;
             expression "unknown\escape"_string;
             expression {123_i8}_map<i8, bool>;
@@ -682,10 +685,17 @@ std::vector<TestCase> getTestCases() {
               "11:23 → Could not parse literal as decimal.",
               "13:26 → Unable to recognize requested type.",
               "14:26 → Unable to recognize requested type.",
-              "16:26 → Unable to recognize requested type.",
-              "17:31 → Unknown slash escape sequence.",
-              "18:23 → Map literals require pairs of values separated by colons.",
-              "19:23 → Map literals require pairs of values separated by colons.",
+              "15:26 → Maps require both a key and a value type.",
+              "15:23 → Unsupported type 0.",
+              "16:26 → Maps require both a key and a value type.",
+              "16:23 → Unsupported type 0.",
+              "17:26 → Unable to recognize requested type.",
+              "18:26 → Unable to recognize requested type.",
+              "18:26 → Unable to recognize requested type.",
+              "19:26 → Unable to recognize requested type.",
+              "20:31 → Unknown slash escape sequence.",
+              "21:23 → Map literals require pairs of values separated by colons.",
+              "22:23 → Map literals require pairs of values separated by colons.",
           }),
       },
       {
