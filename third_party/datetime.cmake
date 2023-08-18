714f486065032230bbaf4e7aaaee88d3a46bf31a
David Sisson <EpsilonPrime@users.noreply.github.com>
2023-08-17 21:13:26 -0700

    move datetime into a package

---
+++ third_party/datetime.cmake
@@ -0,0 +1,13 @@
+# SPDX-License-Identifier: Apache-2.0
+
+include_guard(GLOBAL)
+
+set (BUILD_TZ_LIB ON CACHE BOOL "timezone library is a dependency" FORCE)
+include(FetchContent)
+FetchContent_Declare(date_src
+        GIT_REPOSITORY https://github.com/HowardHinnant/date.git
+        GIT_TAG v3.0.1
+        )
+#if(NOT ${date_src_POPULATED})
+    FetchContent_MakeAvailable(date_src)
+#endif()
