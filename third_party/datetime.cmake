e3e740f0b9ab19b1c9545b9015fde457d34a5334
David Sisson <EpsilonPrime@users.noreply.github.com>
2023-08-21 13:32:53 -0700

    Remove commented out code.

--- third_party/datetime.cmake
+++ third_party/datetime.cmake
@@ -8,6 +8,4 @@ FetchContent_Declare(date_src
         GIT_REPOSITORY https://github.com/HowardHinnant/date.git
         GIT_TAG v3.0.1
         )
-#if(NOT ${date_src_POPULATED})
-    FetchContent_MakeAvailable(date_src)
-#endif()
+FetchContent_MakeAvailable(date_src)
