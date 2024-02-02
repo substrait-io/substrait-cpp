77fcd305d57b70d9419e9b0455c1810587c8ca64
David Sisson <EpsilonPrime@users.noreply.github.com>
2024-02-01 23:19:23 -0800

    Now the library is in a usable state.

--- export/planloader/textplan.cpp
+++ export/planloader/textplan.cpp
@@ -3,30 +3,42 @@
 #include <substrait/common/Io.h>
 
 extern "C" {
-	// Load a Substrait plan (in any format) from disk.
-	// Returns error message on failure, otherwise returns nullptr and
-	// updates the provided buffer.
-	const char* load_substrait_plan(char* planBuffer, int32_t planBufferLen,
-	                                const char* filename) {
-		auto planOrError = io::substrait::loadPlan(filename);
-		if (!planOrError.ok()) {
-			return planOrError.status().message().data();
-		}
-		auto plan = *planOrError;
-		// MEGAHACK -- If space will fit update the planBuffer.
-		return nullptr;
-	}
 
-	// Write a Substrait plan to disk in the specified format.
-	const char* save_substrait_plan(const char* planData, int32_t planDataLen,
-	                                const char* filename,
-	                                io::substrait::PlanFileFormat format) {
-		// MEGAHACK -- Convert the planData into a Plan proto.
-		::substrait::proto::Plan plan;
-		auto result = io::substrait::savePlan(plan, filename, format);
-		if (!result.ok()) {
-			return result.message().data();
-		}
-		return nullptr;
-	}
+// Load a Substrait plan (in any format) from disk.
+// Stores the Substrait plan in planBuffer in serialized form.
+// On error returns a non-empty error message.
+// On success an empty string is returned.
+const char* load_substrait_plan(
+    char* planBuffer,
+    int32_t planBufferLen,
+    const char* filename) {
+  auto planOrError = io::substrait::loadPlan(filename);
+  if (!planOrError.ok()) {
+    return planOrError.status().message().data();
+  }
+  ::substrait::proto::Plan plan = *planOrError;
+  std::string text = plan.SerializeAsString();
+  if (text.length() > planBufferLen) {
+    return "Actual plan size is longer than the provided buffer.";
+  }
+  strncpy(planBuffer, text.c_str(), text.length());
+  return "";
 }
+
+// Write a serialized Substrait plan to disk in the specified format.
+// On error returns a non-empty error message.
+// On success an empty string is returned.
+const char* save_substrait_plan(
+    const char* planData,
+    const char* filename,
+    io::substrait::PlanFileFormat format) {
+  ::substrait::proto::Plan plan;
+  plan.ParseFromString(planData);
+  auto result = io::substrait::savePlan(plan, filename, format);
+  if (!result.ok()) {
+    return result.message().data();
+  }
+  return "";
+}
+
+}  // extern "C"
