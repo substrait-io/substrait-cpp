df1b42c3135dae193dc4356ca138f236d318155e
David Sisson <EpsilonPrime@users.noreply.github.com>
2024-02-02 09:37:13 -0800

    Switch to returning a structure instead of modifying passed in arguments.

--- export/planloader/textplan.cpp
+++ export/planloader/textplan.cpp
@@ -4,25 +4,46 @@
 
 extern "C" {
 
+typedef struct {
+  char *buffer;
+  uint32_t size;
+  char *errorMessage;
+} SerializedPlan;
+
 // Load a Substrait plan (in any format) from disk.
 // Stores the Substrait plan in planBuffer in serialized form.
-// On error returns a non-empty error message.
-// On success an empty string is returned.
-const char* load_substrait_plan(
-    char* planBuffer,
-    int32_t planBufferLen,
-    const char* filename) {
+// Returns a SerializedPlan structure containing either the serialized plan or
+// an error message.
+SerializedPlan* load_substrait_plan(const char* filename) {
+  // MEGAHACK -- Consider putting this initialization into a separate function.
+  auto newPlan = new SerializedPlan();
+  newPlan->buffer = nullptr;
+  newPlan->size = 0;
+  newPlan->errorMessage = nullptr;
+
   auto planOrError = io::substrait::loadPlan(filename);
   if (!planOrError.ok()) {
-    return planOrError.status().message().data();
+    auto errMsg = planOrError.status().message();
+    newPlan->errorMessage = new char[errMsg.length()+1];
+    strncpy(newPlan->errorMessage, errMsg.data(), errMsg.length()+1);
+    return newPlan;
   }
   ::substrait::proto::Plan plan = *planOrError;
   std::string text = plan.SerializeAsString();
-  if (text.length() > planBufferLen) {
-    return "Actual plan size is longer than the provided buffer.";
+  newPlan->buffer = new char[text.length()+1];
+  memcpy(newPlan->buffer, text.data(), text.length()+1);
+  newPlan->size = text.length();
+  return newPlan;
+}
+
+void free_substrait_plan(SerializedPlan* plan) {
+  if (plan->buffer) {
+    free(plan->buffer);
   }
-  strncpy(planBuffer, text.c_str(), text.length());
-  return "";
+  if (plan->errorMessage) {
+    free(plan->errorMessage);
+  }
+  free(plan);
 }
 
 // Write a serialized Substrait plan to disk in the specified format.
@@ -30,10 +51,12 @@ const char* load_substrait_plan(
 // On success an empty string is returned.
 const char* save_substrait_plan(
     const char* planData,
+    uint32_t planDataLength,
     const char* filename,
     io::substrait::PlanFileFormat format) {
   ::substrait::proto::Plan plan;
-  plan.ParseFromString(planData);
+  std::string data(planData, planDataLength);
+  plan.ParseFromString(data);
   auto result = io::substrait::savePlan(plan, filename, format);
   if (!result.ok()) {
     return result.message().data();
