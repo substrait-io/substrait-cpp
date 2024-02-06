74bf673bc9f1e7c603aa69d0035aa35fcde6ea37
David Sisson <EpsilonPrime@users.noreply.github.com>
2024-02-05 22:34:14 -0800

    Added a planloader test.

--- export/planloader/planloader.cpp
+++ export/planloader/planloader.cpp
@@ -1,21 +1,16 @@
 /* SPDX-License-Identifier: Apache-2.0 */
 
+#include "planloader.h"
+
 #include <substrait/common/Io.h>
 
 extern "C" {
 
-typedef struct {
-  char *buffer;
-  uint32_t size;
-  char *errorMessage;
-} SerializedPlan;
-
 // Load a Substrait plan (in any format) from disk.
 // Stores the Substrait plan in planBuffer in serialized form.
 // Returns a SerializedPlan structure containing either the serialized plan or
-// an error message.
+// an error message.  errorMessage is nullptr upon success.
 SerializedPlan* load_substrait_plan(const char* filename) {
-  // MEGAHACK -- Consider putting this initialization into a separate function.
   auto newPlan = new SerializedPlan();
   newPlan->buffer = nullptr;
   newPlan->size = 0;
@@ -48,7 +43,7 @@ void free_substrait_plan(SerializedPlan* plan) {
 
 // Write a serialized Substrait plan to disk in the specified format.
 // On error returns a non-empty error message.
-// On success an empty string is returned.
+// On success a nullptr is returned.
 const char* save_substrait_plan(
     const char* planData,
     uint32_t planDataLength,
@@ -61,7 +56,7 @@ const char* save_substrait_plan(
   if (!result.ok()) {
     return result.message().data();
   }
-  return "";
+  return nullptr;
 }
 
 }  // extern "C"
