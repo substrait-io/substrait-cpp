e49c11ab0b28461dba58a802286cb7b2333c7736
David Sisson <EpsilonPrime@users.noreply.github.com>
2024-02-08 16:40:43 -0800

    Be more explicit about what needs position independent code.

--- third_party/antlr4/cmake/ExternalAntlr4Cpp.cmake
+++ third_party/antlr4/cmake/ExternalAntlr4Cpp.cmake
@@ -6,6 +6,8 @@ endif()
 
 include(ExternalProject)
 
+set(CMAKE_POSITION_INDEPENDENT_CODE TRUE)
+
 set(ANTLR4_ROOT ${CMAKE_CURRENT_BINARY_DIR}/antlr4_runtime/src/antlr4_runtime)
 set(ANTLR4_INCLUDE_DIRS ${ANTLR4_ROOT}/runtime/Cpp/runtime/src)
 set(ANTLR4_GIT_REPOSITORY https://github.com/antlr/antlr4.git)
