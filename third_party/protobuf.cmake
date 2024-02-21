7853b51ea88ae7a725f50a351757e86bb6935af5
David Sisson <EpsilonPrime@users.noreply.github.com>
2024-02-21 02:50:32 -0800

    Now working build with protobuf being provided externally if necessary.

--- third_party/protobuf.cmake
+++ third_party/protobuf.cmake
@@ -5,9 +5,16 @@ include_guard(GLOBAL)
 set(ABSL_PROPAGATE_CXX_STD ON)
 
 include(FetchContent)
+FetchContent_Declare(GTest
+        GIT_REPOSITORY https://github.com/google/googletest.git
+        GIT_TAG v1.14.0
+        OVERRIDE_FIND_PACKAGE
+)
 FetchContent_Declare(Protobuf
         GIT_REPOSITORY https://github.com/protocolbuffers/protobuf.git
-        GIT_TAG v4.25.3
+        GIT_TAG v23.4
+        #SOURCE_SUBDIR cmake
         OVERRIDE_FIND_PACKAGE
-        )
-FetchContent_MakeAvailable(Protobuf)
+)
+set(protobuf_BUILD_TESTS OFF CACHE INTERNAL "")
+FetchContent_MakeAvailable(Protobuf GTest)
