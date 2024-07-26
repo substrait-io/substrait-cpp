e291f762e8c7eee786bab0982722eeafba6bcd68
David Sisson <EpsilonPrime@users.noreply.github.com>
2024-07-25 21:33:57 -0700

    typo fix

--- scripts/run-clang-tidy.sh
+++ scripts/run-clang-tidy.sh
@@ -4,7 +4,7 @@ SCRIPTDIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 WORKDIR="$( cd $SCRIPTDIR/.. && pwd )"
 
 # Make compile_command.json
-export CXXFLAGS="-Wno-attributes -Wno-deprecated-declaraations"
+export CXXFLAGS="-Wno-attributes -Wno-deprecated-declarations"
 rm -rf tmp && mkdir tmp && cmake -Btmp -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DSUBSTRAIT_CPP_ROUNDTRIP_TESTING=ON
 # Build substrait protobuf
 pushd tmp/src/substrait/proto && make -j && popd || exit
