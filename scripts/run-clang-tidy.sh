d47fab625066f0b61548260c58a61c70e19341f6
David Sisson <EpsilonPrime@users.noreply.github.com>
2024-07-25 21:51:35 -0700

    adding diagnostics to the clang-tidy script

--- scripts/run-clang-tidy.sh
+++ scripts/run-clang-tidy.sh
@@ -3,14 +3,16 @@
 SCRIPTDIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 WORKDIR="$( cd $SCRIPTDIR/.. && pwd )"
 
-# Make compile_command.json
-export CXXFLAGS="-Wno-attributes -Wno-deprecated-declarations"
+echo Checking Python version
+which python
+
+echo Making compile_command.json
 rm -rf tmp && mkdir tmp && cmake -Btmp -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DSUBSTRAIT_CPP_ROUNDTRIP_TESTING=ON
-# Build substrait protobuf
+echo Building substrait protobuf
 pushd tmp/src/substrait/proto && make -j && popd || exit
-# Build textplan grammar
+echo Building textplan grammar
 pushd tmp/src/substrait/textplan/parser/grammar && make -j antlr4_runtime textplan_grammar_headers && popd || exit
-# Run clang-tidy
+echo Running clang-tidy
 if [ "$1" == "fix" ]; then
    python3 scripts/run-clang-tidy.py "$WORKDIR" "tmp" "third_party" "h,hpp,cc,cpp" "--quiet --fix"
 else
