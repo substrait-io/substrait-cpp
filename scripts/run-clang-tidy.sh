39af88ecc359939772900a43f65474a691d97369
David Sisson <EpsilonPrime@users.noreply.github.com>
2024-07-25 21:52:56 -0700

    added more detail

--- scripts/run-clang-tidy.sh
+++ scripts/run-clang-tidy.sh
@@ -3,9 +3,6 @@
 SCRIPTDIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 WORKDIR="$( cd $SCRIPTDIR/.. && pwd )"
 
-echo Checking Python version
-which python
-
 echo Making compile_command.json
 rm -rf tmp && mkdir tmp && cmake -Btmp -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DSUBSTRAIT_CPP_ROUNDTRIP_TESTING=ON
 echo Building substrait protobuf
@@ -18,3 +15,4 @@ if [ "$1" == "fix" ]; then
 else
    python3 scripts/run-clang-tidy.py "$WORKDIR" "tmp" "third_party" "h,hpp,cc,cpp" "--quiet"
 fi
+echo Finished running clang-tidy
