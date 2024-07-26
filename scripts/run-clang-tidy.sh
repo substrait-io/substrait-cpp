0f7072c8ed7ce92253d026496351de27ffd18580
David Sisson <EpsilonPrime@users.noreply.github.com>
2024-07-25 21:59:40 -0700

    reorganized

--- scripts/run-clang-tidy.sh
+++ scripts/run-clang-tidy.sh
@@ -3,10 +3,14 @@
 SCRIPTDIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 WORKDIR="$( cd $SCRIPTDIR/.. && pwd )"
 
+set -o errexit
+
 echo Making compile_command.json
 rm -rf tmp && mkdir tmp && cmake -Btmp -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DSUBSTRAIT_CPP_ROUNDTRIP_TESTING=ON
 echo Building substrait protobuf
-pushd tmp/src/substrait/proto && make -j && popd || exit
+pushd tmp/src/substrait/proto
+make -j
+popd
 echo Building textplan grammar
 pushd tmp/src/substrait/textplan/parser/grammar && make -j antlr4_runtime textplan_grammar_headers && popd || exit
 echo Running clang-tidy
