2d69be3ff704103a539c266a8aa942a529832819
Morten Borup Petersen <morten_bp@live.dk>
2025-03-03 14:03:29 +0100

    NFC

--- src/substrait/textplan/converter/LoadBinary.h
+++ src/substrait/textplan/converter/LoadBinary.h
@@ -12,8 +12,7 @@ class Plan;
 
 namespace io::substrait::textplan {
 
-// Read the contents of a file from disk. 'forceBinary' enables file reading in
-// binary mode.
+// Read the contents of a file from disk.
 absl::StatusOr<std::string> readFromFile(std::string_view msgPath);
 
 // Reads a plan from a json-encoded text proto.
