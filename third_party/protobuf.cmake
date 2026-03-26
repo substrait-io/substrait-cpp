d3588e8681af796a6aba083268db5bd685a44f76
Ingo Müller <ingomueller@google.com>
2026-03-26 17:40:20 +0100

    style(cmake): improve cmake-format script and apply format (#143)
    This PR improves the `run-cmake-format.sh` script by using `git
    ls-files` instead of using `find ... | grep -v`. Crucially, this
    automatically excludes build files and external dependencies
    automatically. With the previous script, the `find` command would
    descend into the build folder and take forever. The new command is also
    more targetted and includes a few CMake scripts that are part of this
    repository but were previously (wrongly) excluded. Since these files did
    not adhere to the format previously, the PR also reformats them.
    
    Signed-off-by: Ingo Müller <ingomueller@google.com>

--- third_party/protobuf.cmake
+++ third_party/protobuf.cmake
@@ -5,20 +5,21 @@ include_guard(GLOBAL)
 set(ABSL_PROPAGATE_CXX_STD ON)
 
 include(FetchContent)
-FetchContent_Declare(GTest
-        GIT_REPOSITORY https://github.com/google/googletest.git
-        GIT_TAG v1.14.0
-        OVERRIDE_FIND_PACKAGE
-)
-FetchContent_Declare(Protobuf
-        GIT_REPOSITORY https://github.com/protocolbuffers/protobuf.git
-        GIT_TAG v29.3
-        SYSTEM
-        OVERRIDE_FIND_PACKAGE
-)
+fetchcontent_declare(
+  GTest
+  GIT_REPOSITORY https://github.com/google/googletest.git
+  GIT_TAG v1.14.0
+  OVERRIDE_FIND_PACKAGE)
+fetchcontent_declare(
+  Protobuf
+  GIT_REPOSITORY https://github.com/protocolbuffers/protobuf.git
+  GIT_TAG v29.3
+  SYSTEM OVERRIDE_FIND_PACKAGE)
 
 # Disable warnings for dependency targets.
-set(protobuf_BUILD_TESTS OFF CACHE INTERNAL "")
+set(protobuf_BUILD_TESTS
+    OFF
+    CACHE INTERNAL "")
 if(MSVC)
   set(protobuf_MSVC_STATIC_RUNTIME OFF)
   set(gtest_force_shared_crt ON)
@@ -26,4 +27,4 @@ if(MSVC)
 else()
   add_compile_options("-w")
 endif()
-FetchContent_MakeAvailable(Protobuf GTest)
+fetchcontent_makeavailable(Protobuf GTest)
