283f0a2c163149a9eb13a39ec92018baa01817a3
Ingo Müller <ingomueller@google.com>
2026-03-26 09:26:31 +0100

    build(protobuf): refactor CMake files for finding Protobuf
    This PR refactors the CMake logic that finds or fetches the protobuf
    dependency. This consists of the following individual changes:
    
    * Move the fetching of the `googletest` dependency from `protobuf.cmake`
      to `gtest.cmake` because it is unrelated.
    * Introduce options that allow to disable the usage of `find_package`
      and `FetchContent` respectively in order to give downstream projects
      control on how the dependency is provided.
    * Introduce options to enable or disable config mode for `find_package`
      and the git tag used by `FetchContent` for the same reason.
    
    Signed-off-by: Ingo Müller <ingomueller@google.com>

--- third_party/protobuf.cmake
+++ third_party/protobuf.cmake
@@ -2,29 +2,67 @@
 
 include_guard(GLOBAL)
 
-set(ABSL_PROPAGATE_CXX_STD ON)
+option(SUBSTRAIT_CPP_USE_SYSTEM_PROTOBUF "Use system Protobuf via find_package"
+       ON)
+option(SUBSTRAIT_CPP_FIND_PROTOBUF_CONFIG
+       "Use CONFIG mode to find system Protobuf via find_package" ON)
+option(SUBSTRAIT_CPP_FETCH_PROTOBUF
+       "Download Protobuf via FetchContent if not found" ON)
+set(SUBSTRAIT_CPP_PROTOBUF_FETCH_TAG
+    ${SUBSTRAIT_CPP_PROTOBUF_DEFAULT_FETCH_TAG}
+    CACHE STRING "Git tag or commit to use for Protobuf FetchContent")
 
-include(FetchContent)
-fetchcontent_declare(
-  GTest
-  GIT_REPOSITORY https://github.com/google/googletest.git
-  GIT_TAG v1.14.0
-  OVERRIDE_FIND_PACKAGE)
-fetchcontent_declare(
-  Protobuf
-  GIT_REPOSITORY https://github.com/protocolbuffers/protobuf.git
-  GIT_TAG v29.3
-  SYSTEM OVERRIDE_FIND_PACKAGE)
-
-# Disable warnings for dependency targets.
-set(protobuf_BUILD_TESTS
-    OFF
-    CACHE INTERNAL "")
-if(MSVC)
-  set(protobuf_MSVC_STATIC_RUNTIME OFF)
-  set(gtest_force_shared_crt ON)
-  add_compile_options("/W0")
-else()
-  add_compile_options("-w")
+# First use `find_package`. This allows downstream projects to inject their
+# version with `FetchContent_Declare(... OVERRIDE_FIND_PACKAGE`.
+if(SUBSTRAIT_CPP_USE_SYSTEM_PROTOBUF)
+  if(SUBSTRAIT_CPP_FIND_PROTOBUF_CONFIG)
+    find_package(Protobuf CONFIG)
+  else()
+    find_package(Protobuf)
+  endif()
 endif()
-fetchcontent_makeavailable(Protobuf GTest)
+
+# Now fall back to using `FetchContent`.
+if(NOT Protobuf_FOUND AND SUBSTRAIT_CPP_FETCH_PROTOBUF)
+  message(
+    STATUS "Fetching Protobuf version ${SUBSTRAIT_CPP_PROTOBUF_FETCH_TAG}")
+  include(FetchContent)
+  set(protobuf_BUILD_TESTS
+      OFF
+      CACHE BOOL "Disable Protobuf tests" FORCE)
+  fetchcontent_declare(
+    protobuf
+    GIT_REPOSITORY https://github.com/protocolbuffers/protobuf.git
+    GIT_TAG ${SUBSTRAIT_CPP_PROTOBUF_FETCH_TAG}
+    SYSTEM OVERRIDE_FIND_PACKAGE)
+  set(protobuf_BUILD_TESTS
+      OFF
+      CACHE INTERNAL "")
+  set(ABSL_PROPAGATE_CXX_STD ON)
+  if(MSVC)
+    set(protobuf_MSVC_STATIC_RUNTIME OFF)
+    add_compile_options("/W0")
+  else()
+    add_compile_options("-w")
+  endif()
+  fetchcontent_makeavailable(protobuf)
+  fetchcontent_getproperties(Protobuf SOURCE_DIR Protobuf_SOURCE_DIR)
+  # This exists from Protobuf version 22.0 and up.
+  include(${Protobuf_SOURCE_DIR}/cmake/protobuf-generate.cmake)
+  if(TARGET libprotobuf AND NOT TARGET protobuf::libprotobuf)
+    add_library(protobuf::libprotobuf ALIAS libprotobuf)
+  endif()
+  if(TARGET protoc AND NOT TARGET protobuf::protoc)
+    add_executable(protobuf::protoc ALIAS protoc)
+  endif()
+endif()
+
+if(NOT TARGET protobuf::libprotobuf)
+  message(FATAL_ERROR "Protobuf is required but was not found or fetched.")
+endif()
+
+if(NOT TARGET absl::status)
+  set(ABSL_INCLUDED_WITH_PROTOBUF OFF)
+else()
+  set(ABSL_INCLUDED_WITH_PROTOBUF ON)
+endif()
