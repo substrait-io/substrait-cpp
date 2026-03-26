74fcf18c58c6e43f7f04e53c174172fcad270302
Ingo Müller <ingomueller@google.com>
2026-03-26 17:40:23 +0100

    build(clang-tidy): replace clang-tidy scripts with CMake approach (#142)
    This PR replaces the clang-tidy scripts with a CMake-based approach. The
    core mechanism is to define CMake's `CMAKE_CXX_CLANG_TIDY` variable,
    which invokes clang-tidy whenever a C++ target is compiled. The
    remainder consists of some logic that finds the clang-tidy executable
    and its arguments as well as activating and deactivating the command in
    the right place (in particular, to exclude checking third-party code).
    
    I see three main advantages to the new approach: (1) The new logic is
    considerably simpler than the previous scripts. (2) The code is
    automatically checked when it compiled, potentially saving round-trips
    through CI. (3) We save an extra 10 minutes on CI for recompiling the
    whole project, which the previous scripts did.
    
    The PR also does two minor changes: (1) It removes the
    `AnalyzeTemporaryDtors` configuration, which was depcrecated in clang 16
    and removed in clang 18. (2) It applies the suggested fixes for one
    class of errors that my clang-tidy-22 complained about.
    
    Signed-off-by: Ingo Müller <ingomueller@google.com>

---
+++ cmake/clang-tidy.cmake
@@ -0,0 +1,31 @@
+# SPDX-License-Identifier: Apache-2.0
+
+option(SUBSTRAIT_CPP_ENABLE_CLANG_TIDY
+       "Enable clang-tidy static analysis on Substrait MLIR targets" OFF)
+option(SUBSTRAIT_CPP_CLANG_TIDY_PATH "path to 'clang-tidy' executable")
+
+if(SUBSTRAIT_CPP_ENABLE_CLANG_TIDY)
+  # Find `clang-tidy` executable.
+  get_filename_component(CLANG_TIDY_NAME "${SUBSTRAIT_CPP_CLANG_TIDY_PATH}"
+                         NAME)
+  get_filename_component(CLANG_TIDY_DIR "${SUBSTRAIT_CPP_CLANG_TIDY_PATH}"
+                         DIRECTORY)
+  find_program(
+    CLANG_TIDY_EXE
+    HINTS ${CLANG_TIDY_DIR}
+    NAMES "clang-tidy" "${CLANG_TIDY_NAME}" REQUIRED)
+  if("${CLANG_TIDY_EXE}" STREQUAL "")
+    message(FATAL_ERROR "failed to find clang-tidy executable")
+  endif()
+
+  # Assemble command line.
+  set(CLANG_TIDY_CONFIG_FILE "${CMAKE_SOURCE_DIR}/.clang-tidy")
+  set(CLANG_TIDY_COMMAND
+      "${CLANG_TIDY_EXE};--config-file=${CLANG_TIDY_CONFIG_FILE}")
+  set(CMAKE_CXX_CLANG_TIDY_EXPORT_FIXES_DIR
+      "${CMAKE_BINARY_DIR}/clang-tidy-fixes")
+  message(STATUS "clang-tidy enabled, command: ${CLANG_TIDY_COMMAND}")
+endif(SUBSTRAIT_CPP_ENABLE_CLANG_TIDY)
+
+# Enable clang-tidy for all C++ targets.
+set(CMAKE_CXX_CLANG_TIDY ${CLANG_TIDY_COMMAND})
