147a362210fce4d1f95c06783962b97ab0edfbfc
Ingo Müller <ingomueller@google.com>
2026-04-09 14:19:40 +0200

    build(deps): refactor CMake files for finding GTest (#140)
    This PR refactors the CMake logic that finds or fetches the
    GTest/googletest dependency. It follows the same pattern applied by #138
    for the protobuf library. The PR also removes a redundant logic of
    finding/fetching that dependency, which previously existed twice.
    
    Signed-off-by: Ingo Müller <ingomueller@google.com>

--- third_party/gtest.cmake
+++ third_party/gtest.cmake
@@ -2,18 +2,54 @@
 
 include_guard(GLOBAL)
 
-include(FetchContent)
-fetchcontent_declare(
-  GTest
-  GIT_REPOSITORY https://github.com/google/googletest.git
-  GIT_TAG v1.14.0
-  OVERRIDE_FIND_PACKAGE)
+option(SUBSTRAIT_CPP_USE_SYSTEM_GTEST "Use system GTest via find_package" ON)
+option(SUBSTRAIT_CPP_FIND_GTEST_CONFIG
+       "Use CONFIG mode to find system GTest via find_package" ON)
+option(SUBSTRAIT_CPP_FETCH_GTEST "Download GTest via FetchContent if not found"
+       ON)
+set(SUBSTRAIT_CPP_GTEST_FETCH_TAG
+    ${SUBSTRAIT_CPP_GTEST_DEFAULT_FETCH_TAG}
+    CACHE STRING "Git tag or commit to use for GTest FetchContent")
 
-# Disable warnings for dependency targets.
-if(MSVC)
-  set(gtest_force_shared_crt ON)
-  add_compile_options("/W0")
-else()
-  add_compile_options("-w")
+# First use `find_package`. This allows downstream projects to inject their
+# version with `FetchContent_Declare(... OVERRIDE_FIND_PACKAGE`.
+if(SUBSTRAIT_CPP_USE_SYSTEM_GTEST)
+  if(SUBSTRAIT_CPP_FIND_GTEST_CONFIG)
+    find_package(GTest CONFIG)
+  else()
+    find_package(GTest)
+  endif()
 endif()
-fetchcontent_makeavailable(GTest)
+
+# Now fall back to using `FetchContent`.
+if(NOT GTest_FOUND AND SUBSTRAIT_CPP_FETCH_GTEST)
+  message(STATUS "Fetching googletest version ${SUBSTRAIT_CPP_GTEST_FETCH_TAG}")
+  include(FetchContent)
+  fetchcontent_declare(
+    googletest
+    GIT_REPOSITORY https://github.com/google/googletest.git
+    GIT_TAG ${SUBSTRAIT_CPP_GTEST_FETCH_TAG}
+    SYSTEM OVERRIDE_FIND_PACKAGE CMAKE_ARGS
+    -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+    -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER})
+  if(MSVC)
+    set(gtest_force_shared_crt
+        ON
+        CACHE BOOL "" FORCE)
+    add_compile_options("/W0")
+  else()
+    add_compile_options("-w")
+  endif()
+  fetchcontent_makeavailable(googletest)
+  fetchcontent_getproperties(googletest SOURCE_DIR GTest_SOURCE_DIR)
+  if(TARGET gmock AND NOT TARGET GTest::gmock)
+    add_library(GTest::gmock ALIAS gmock)
+  endif()
+  if(TARGET gmock_main AND NOT TARGET GTest::gmock_main)
+    add_library(GTest::gmock_main ALIAS gmock_main)
+  endif()
+endif()
+
+if(NOT TARGET GTest::gmock)
+  message(FATAL_ERROR "GTest is required but was not found or fetched.")
+endif()
